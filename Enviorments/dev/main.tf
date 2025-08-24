23e9ff70a026d8112a669fc4d404d5f702ba5ca8
Priya Patel <Swara13dec1997@gmail.com>
2025-08-25 00:47:47 +0530

    commited workflow file

--- Enviorments/dev/main.tf
+++ Enviorments/dev/main.tf
@@ -1,5 +1,5 @@
 module "rg1" {
-    source = "../modules/1_azurerm_resource_group"
+    source = "../../modules/1_azurerm_resource_group"
     rg_name     = "test-rg-todo"
     rg_location = "West Europe"
   
